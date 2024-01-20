38980e4bfae656217ee16356178e3d5454ba35d5
ERFAN POURNASIRI <erfanpournasiri71@gmail.com>
2024-01-20 08:44:01 +0330

    create page folder and make routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import {Home,Projects,About,SignIn,SignUp} from './index.js'
 
 function App() {
   return (
-    <div className='text-3xl'>App</div>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<SignIn/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+      </Routes>
   )
 }
 
---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from './pages/Home'
+export {default as Projects} from './pages/Projects'
+export {default as About} from './pages/About'
+export {default as SignIn} from './pages/SignIn'
+export {default as SignUp} from './pages/SignUp'
