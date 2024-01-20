64267b705d71056e0cb5acb257d4667e17d641aa
ERFAN POURNASIRI <erfanpournasiri71@gmail.com>
2024-01-20 12:53:16 +0330

    create controllers folder and user controller

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const users = (req, res) => {
+    res.json({message:"users route is working correctly 😀"})
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,8 @@
 import express from 'express'
+import { users } from '../controllers/user.controller.js';
 
 const router = express.Router()
 
-router.get('/users', (req, res) => {
-    res.json({message:"users route is working correctly 😀"}) 
-})
+router.get('/users', users );
 
 export default router
