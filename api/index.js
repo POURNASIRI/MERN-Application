288d621a6a6f0abf754c892bd60ebfea0e7d3699
ERFAN POURNASIRI <erfanpournasiri71@gmail.com>
2024-01-21 10:02:11 +0330

    create signup route and controller

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import users from './routes/user.route.js'
+import signup from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO)
 })
 
 const app = express()
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
@@ -21,3 +23,4 @@ app.listen(3000, () => {
 
 
 app.use('/api/user/',users)
+app.use('/api/auth', signup)
